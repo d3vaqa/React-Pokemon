ec6be1760a63e558201778d86a007a35a1e9c52f
Hiro <d3vaqa@gmail.com>
2023-10-07 17:06:25 +1100

    Pokemon cards without logics

---
+++ src/Components/Pokecard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Pokecard.css'
+
+const   POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/"
+const Pokecard = (props) => {
+    let imgSrc = `${POKE_API}${props.id}.png`;
+
+  return (    
+    <div className='Pokecard'>
+        <h1 className='Pokecard-title'>{props.name}</h1>
+        <img src={imgSrc} alt={props.name} />
+        <div className='Pokecard-data'>Type: {props.type}</div>
+        <div className='Pokecard-data'>Exp: {props.exp}</div>
+
+    </div>
+  )
+}
+
+export default Pokecard
